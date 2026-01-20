a5afdbab54b017f476b63d1aca2bc2802d477159
ankit <ankit8104.ar@gmail.com>
2026-01-21 04:52:59 +0530

    added new branch nginx-log-analyser, adding script file for same

---
+++ nginx_log_analyser.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+LOG_FILE=$1
+
+echo "Top 5 IP addresses with the most requests:"
+awk '{print $1}' $LOG_FILE \
+	| sort \
+	| uniq -c \
+	| sort -nr \
+	| head -5 \
+	| awk '{printf "%s - %d requests\n", $2, $1}'
+echo
+
+echo "Top 5 most requested paths"
+awk -F\" '{print $2}' $LOG_FILE \
+	| sort \
+	| uniq -c \
+	| sort -nr \
+	| head -5 \
+	| awk '{printf "%s - %d requests\n", $3,  $1}'
+echo
+
+echo "Top 5 most response status codes"
+awk '{print $9}' $LOG_FILE \
+	| sort \
+	| uniq -c \
+	| sort -nr \
+	| head -5 \
+	| awk '{printf "%s - %d requests\n", $2,  $1}'
+echo
+
+awk '
+{
+    count[$9]++
+}
+END {
+    for (code in count)
+        printf "%s - %d requests\n", code, count[code]
+}
+' "$LOG_FILE" \
+| sort -t'-' -k2 -nr \
+| head -5
